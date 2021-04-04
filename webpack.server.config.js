67812bcb7fe4f5e9ffc91b868f0aae0f60d7e60c
Andrew A. Anissi <andrew@wingedearth.com>
2021-04-03 17:36:18 -0700

    feat: add webpack build with js loader

---
+++ webpack.server.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const getRules = require('./webpack/rules');
+
+const config = (variables) => {
+  const { devtool, mode, watch } = variables;
+
+  return {
+    devtool,
+    mode,
+    watch,
+    entry: {
+      server: path.join(__dirname, 'src', 'server', 'server.js')
+    },
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'dist', 'server')
+    },
+    resolve: {
+      alias: {
+        '@': path.join(__dirname, 'src')
+      },
+      extensions: ['.js', '.json', 'jsx'],
+      modules: ['node_modules']
+    },
+    module: {
+      rules: getRules()
+    },
+    target: 'node',
+    externals: [
+      nodeExternals({
+        allowlist: ['webpack/hot/dev-server', /\.(?!(?:jsx?|json)$).{1,5}$/i]
+      })
+    ]
+  };
+};
+
+module.exports = config;
